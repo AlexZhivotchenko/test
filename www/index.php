b2a71c1d8cb0c348f4106c763e2b02ea89b97f1e
Алексей Животченко < >
2017-03-04 17:29:12 +0200

    Add Class1

--- www/index.php
+++ www/index.php
@@ -1,5 +1,25 @@
 <?php
+echo
+ class ModelSQL {
+     public $con;
+     public function  __construct()
+     {
+         return $this->con = new PDO('mysql:host=127.0.0.1;dbname=userlistdb', 'root', '');
+     }
+     public function addNewPost($nameTable, $value) {
 
-phpinfo(); 
+         $sql = ($this->con)->prepare('INSERT INTO '.$nameTable.' (nameCountry) VALUES (?)');
+         var_dump($sql);
+         return $sql->execute(array($value));
 
-?>
+     }
+     public function updatePost($nameTable, $valueSET, $valueLike) {
+
+         $sql1 = ($this->con)->prepare('UPDATE '.$nameTable.'  SET email = ?  WHERE nameCountry LIKE ?');
+         return $sql1->execute(array($valueSET, $valueLike));
+
+     }
+ }
+ $value1 = new ModelSQL();
+ $value1->addNewPost('country', 'USSR');
+ $value1->updatePost('usertbl', 'mis@mail.ru', 'Belgium');
