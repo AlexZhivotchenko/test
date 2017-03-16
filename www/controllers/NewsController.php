75c7abc21bb72789c2cfe2af9feca252f25ed0d1
AlexZhivotchenko <qwer75319>
2017-03-16 20:54:16 +0200

    mvc.Full!!

--- www/controllers/NewsController.php
+++ www/controllers/NewsController.php
@@ -1,18 +1,25 @@
 <?php
 
 require_once  __DIR__ . '/../models/News.php';
+require_once  __DIR__ . '/../classes/View.php';
 
 class NewsController
 {
     public function actionAll()
     {
-        $items = News::getAll();
-        include __DIR__.'/../views/news/all.php';
+        $news = News::getAll();
+        $view = new View();
+//        $view->assign('items', $news);
+        $view->items = $news;
+        $view->display('news/all.php');
     }
+    
     public function actionOne()
     {
         $id = $_GET['id'];
         $item = News::getOne($id);
-        include __DIR__.'/../views/news/one.php';
+        $view = new View();
+        $view->assign('item', $item);
+        $view->display('news/one.php');
     }
 }
